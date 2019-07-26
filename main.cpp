27b9b6bcb3181df19f61d803ce3535e78b3c9709
qudwns2052 <qudwns2052@gmail.com>
2019-07-26 05:21:49 -0700

    190726

--- main.cpp
+++ main.cpp
@@ -2,117 +2,93 @@
 #include <stdio.h>
 #include "packet_structure.h"
 #include <arpa/inet.h>
+#include <iostream>
 
-void usage() {
-  printf("syntax: pcap_test <interface>\n");
-  printf("sample: pcap_test wlan0\n");
-}
+#define ETHER_HEADER_SIZE   14
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
 
-void print_d_Mac(Ethernet * e)
+using namespace std;
+
+void print_info(Ethernet * e, Ip * ip, Tcp * tcp)
 {
     printf("d_mac = %02X:%02X:%02X:%02X:%02X:%02X\n", e->d_mac[0],e->d_mac[1],e->d_mac[2],e->d_mac[3],e->d_mac[4],e->d_mac[5]);
-}
-
-void print_s_Mac(Ethernet * e)
-{
     printf("s_mac = %02X:%02X:%02X:%02X:%02X:%02X\n", e->s_mac[0],e->s_mac[1],e->s_mac[2],e->s_mac[3],e->s_mac[4],e->s_mac[5]);
-}
-
-void print_s_ip(Ip * ip)
-{
     printf("s_ip = %u.%u.%u.%u\n", ip->s_ip[0], ip->s_ip[1], ip->s_ip[2], ip->s_ip[3]);
-}
-
-void print_d_ip(Ip * ip)
-{
     printf("d_ip = %u.%u.%u.%u\n", ip->d_ip[0], ip->d_ip[1], ip->d_ip[2], ip->d_ip[3]);
+    cout << "s_port = " << htons(tcp->s_port) << endl;
+    cout << "d_port = " << htons(tcp->d_port) << endl;
+    printf("----------------TCP Packet--------------\n");
 }
 
-void print_s_port(Tcp * tcp)
+
+int main(int argc, char* argv[])
 {
-    printf("s_port = %d\n", ntohs(tcp->s_port));
-}
 
-void print_d_port(Tcp * tcp)
-{
-    printf("d_port = %d\n", ntohs(tcp->d_port));
-}
+    char* dev = argv[1];
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 
-
-
-
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    usage();
-    return -1;
-  }
-
-  char* dev = argv[1];
-  char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-  if (handle == NULL) {
-    fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
-    return -1;
-  }
-
-
-  int ethernet_SIZE;
-  int ip_SIZE;
-  int tcp_SIZE;
-  int payload_SIZE;
-
-  while (true) {
-    struct pcap_pkthdr* header;
-    const u_char* packet;
-    int res = pcap_next_ex(handle, &header, &packet);
-
-    if (res == 0) continue;
-    if (res == -1 || res == -2) break;
-
-    Ethernet * ethernet_header = (Ethernet *)packet;
-    ethernet_SIZE = 14;
-
-    Ip * ip_header = (Ip *)(packet + ethernet_SIZE);
-    ip_SIZE = (ip_header->VHL & 0x0F) * 4;
-
-    Tcp * tcp_header = (Tcp *)(packet + ethernet_SIZE + ip_SIZE);
-    tcp_SIZE = ((tcp_header->HLR & 0xF0) >> 4) * 4;
-
-    u_char * payload = (u_char *)(packet + ethernet_SIZE + ip_SIZE + tcp_SIZE);
-    payload_SIZE = ntohs(ip_header->Total_LEN) - (ip_SIZE + tcp_SIZE);
-
-    if(ethernet_header->type == ntohs(0x0800) &&
-            ip_header->protocol == 0x06)
-    {
-        printf("\n------------------------------------\n\n");
-
-        print_d_Mac(ethernet_header);
-        print_s_Mac(ethernet_header);
-        print_s_ip(ip_header);
-        print_d_ip(ip_header);
-        print_s_port(tcp_header);
-        print_d_port(tcp_header);
-
-        if(payload_SIZE == 0)   printf("payload data : X\n");
-        else
-        {
-            printf("payload data : ");
-            for(int i=0; i<payload_SIZE;i++)
-            {
-                if(i >= 10)
-                    break;
-                printf("%02X ", payload[i]);
-            }
-            printf("\n");
-        }
-
-        printf("%u bytes captured\n", header->caplen);
-        printf("\n------------------------------------\n");
+    if (handle == NULL) {
+        fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+        return -1;
     }
 
 
-  }
+    int ethernet_SIZE;
+    int ip_SIZE;
+    int tcp_SIZE;
+    int payload_SIZE;
 
-  pcap_close(handle);
-  return 0;
+    while (true)
+    {
+        printf("\n\n--------------Packet Capture-------------\n");
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(handle, &header, &packet);
+
+        if (res == 0) continue;
+        if (res == -1 || res == -2) break;
+
+        Ethernet * ethernet_header = (Ethernet *)packet;
+        ethernet_SIZE = 14;
+
+        if(ntohs(ethernet_header->type) != 0x0800)
+            continue;
+
+        Ip * ip_header = (Ip *)(packet + ethernet_SIZE);
+        ip_SIZE = (ip_header->VHL & 0x0F) * 4;
+
+        if(ip_header->protocol != 0x06)
+            continue;
+        Tcp * tcp_header = (Tcp *)(packet + ethernet_SIZE + ip_SIZE);
+        tcp_SIZE = ((tcp_header->HLR & 0xF0) >> 4) * 4;
+
+        u_char * payload = (u_char *)(packet + ethernet_SIZE + ip_SIZE + tcp_SIZE);
+        payload_SIZE = ntohs(ip_header->Total_LEN) - (ip_SIZE + tcp_SIZE);
+
+        cout << "\n----------------Filtering---------------\n" << endl;
+        //
+
+
+
+
+
+
+
+
+
+
+
+        //
+        print_info(ethernet_header, ip_header, tcp_header);
+        printf("----------------TCP Packet--------------\n");
+    }
+
+    pcap_close(handle);
+    return 0;
 }
--- packet_structure.h
+++ packet_structure.h
@@ -18,7 +18,8 @@ typedef struct ip
     uint8_t VHL;
     uint8_t TOS;
     uint16_t Total_LEN;
-    uint32_t something;
+    uint16_t Id;
+    uint16_t Fragment;
     uint8_t TTL;
     uint8_t protocol;
     uint16_t checksum;
@@ -33,7 +34,8 @@ typedef struct tcp
     uint32_t seq;
     uint32_t ack;
     uint8_t HLR;
-    uint8_t something;
-    uint16_t something2;
-    uint32_t something3;
+    uint8_t flags;
+    uint16_t win_size;
+    uint16_t check_sum;
+    uint16_t urg_pointer;
 }Tcp;
